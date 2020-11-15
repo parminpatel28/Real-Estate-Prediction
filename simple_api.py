38a8ad3ce68ccb0c2559b7a0e949ba093436add7
Parmin <parmin@Parmins-MacBook-Pro.local>
2020-11-15 12:13:05 -0500

    first commit

---
+++ simple_api.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+from flask import Flask, request 
+app = Flask(__name__)
+
+
+# In[2]:
+
+
+@app.route('/') 
+def hello_world(): 
+ return 'This is the Index page'
+
+
+# In[3]:
+
+
+@app.route('/add', methods=['POST']) 
+def add(): 
+    req_data = request.get_json() 
+    number_1 = req_data['number_1'] 
+    number_2 = req_data['number_2'] 
+    return str(int(number_1)+int(number_2))
+
